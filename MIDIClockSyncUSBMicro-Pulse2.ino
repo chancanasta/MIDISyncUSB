d2111c986b404d6ece9795df0a1a3cebf9c025f5
chancanasta <brian.lewis1@gmail.com>
2026-05-11 21:15:44 +0100

    Rewrite to use a square wave duty cyle
    Changed to generate square waves of a duty cycle based on gaps between MIDI clocks, rather than generating n pulses from a MIDI clock signal

--- MIDIClockSyncUSBMicro-Pulse2.ino
+++ MIDIClockSyncUSBMicro-Pulse2.ino
@@ -4,54 +4,54 @@
 
 #define TRUE  1
 #define FALSE 0
-
+//available sync ouputs (PPQNs)
 #define SYNC_24 0
 #define SYNC_48 1
 #define SYNC_96 2
 
-//this is the min gap in ms between a 24ppq clock we'll support
-//whoch will allow us to work up to ~250 bpm
-#define DEFAULT_GAP 15
+//our guess at a temp (BPM) if no previous clock signals recieved
+#define BPM_GUESS 160
+
 //MIDI baud rate
 #define USB_MIDI_BAUD 115200
+//gap at end of full cycle (in ms) to allow next MIDI clock to trigger square
+//rather than it auto triggering before next clock signal  
+#define SQUARE_BUFFER 2
+
 
 //number of MIDI clocks per quarter note
 #define MIDI_CLOCKS 24
 
+//number of ms to keep LED on for a "flash"
+#define LED_FLASH_TIME  20
 
-
-//number of loops to keep LED on for a "flash"
-#define LED_COUNT  500
-#define DEBUG_FLASH_COUNT 8000
 
 //bunch of globals for the clock
 byte gClockSetting=0;
-byte gClockMultiplier=1;
-byte gTickSetting=0;
-byte gClockCount=0;
 byte gClockRunning=FALSE;
-byte gSendDebugCount=0;
-byte gRunningClock=FALSE;
 
-//a few more
-unsigned long gLastMilli=0;
-unsigned long gMilliGap=0;
-unsigned long gLastOutTick=0;
-unsigned long gOutTickGap=0;
-byte gGotInTick=0;
-byte gOutTickCount;
+
+//Variables for square wave and duty cycle
+byte gPulseHigh=FALSE;
+unsigned long gDutyCycle=0;
+unsigned long gPulseStart=0;
+unsigned long gFullCycle=0;
+byte gSquareRunning=FALSE;
+byte gTriggerSquare=FALSE;
+unsigned long lastFlash=0;
+unsigned long gGuessClock=0;
+unsigned long gLastMIDIClock=0;
+unsigned long gNow;
+
+//number of pulses we'll send per MIDI clock
+byte gPulsesPerMIDI=1;
+byte gCurrentNoPulses=0;
 //LED flash
-byte gFlashCnt=0;
 byte gLEDOn=FALSE;
-unsigned long gLEDCount=0;
-unsigned long gWaitLoop=0; 
+unsigned long gLEDOffTime=0;
 
-//pulse
-byte gPulseOn=FALSE;
-#define PULSE_LENGTH  2
-unsigned long gPulseEnd=0;
+//PIN for square wave output
 #define CLOCK_PIN 7
-
 
 
 #ifdef DEBUG_OUTPUT
@@ -64,22 +64,6 @@ void setup()
 //set PPQ (24,48 or 96)
   gClockSetting=SYNC_48;
 
-//set the multiplier for the output clock
-  switch(gClockSetting)
-  {
-    case SYNC_48:
-      gClockMultiplier=2; //48ppq (Linn etc)
-      break;
-    case SYNC_96:
-      gClockMultiplier=4; //96ppq (Oberheim etc)
-      break;
-    case SYNC_24:
-    default:
-      gClockMultiplier=1; //24ppq (Roland etc)
-
-  }
-  gTickSetting=gClockMultiplier*MIDI_CLOCKS;
-
 //LED setup
   pinMode(LED_BUILTIN,OUTPUT);   // declare the LED's pin as output
   pinMode(LED_BUILTIN_TX,INPUT); //turn off built in transmit LED on miro board
@@ -87,65 +71,41 @@ void setup()
   digitalWrite(CLOCK_PIN,LOW);
 
 
-//MIDI in setup
+//USB MIDI in setup
   Serial.begin(USB_MIDI_BAUD);  //start serial with usb midi baudrate 
   Serial.flush();
 #ifdef DEBUG_OUTPUT
   Serial.print("Starting\r\n");
 #endif
+//create a guess square wave cycle time
+//this will be used if no previous clocks have been recieved
+  gGuessClock=(60000/BPM_GUESS)/24;
+
+// set a 200ms second duty cycle as a test
+//  gDutyCycle=200;
+//  gFullCycle=gDutyCycle<<1;
+//  gPulseHigh=FALSE;
+ // gClockRunning=TRUE;
+ // gTriggerSquare=TRUE;
+  gLastMIDIClock=0;
+  setPulse();
 }
 
-void loop() 
+void loop()
 {
-  midiEventPacket_t rx;
+//MIDI event packet
+   midiEventPacket_t rx;
 
-//debug - just flash the LED and put out a square wave to show we're alive
-#ifdef DEBUG_OUTPUT
-  gWaitLoop++;
-  if( gWaitLoop>DEBUG_FLASH_COUNT)
-  {
-    gWaitLoop=0;
-    flash();
-    //Pulse
-    sendPulse();
-  }
-#endif
-//are we sending square wave?
-  if(gPulseOn)
-  {
-    if(gPulseEnd==0)
-    {
-      gPulseEnd=(millis()+PULSE_LENGTH);
-      digitalWrite(CLOCK_PIN,HIGH);
-    }
-    else
-    {
-//turn off output if we've hit end of pulse
-      if(millis()>gPulseEnd)
-      {
-        gPulseOn=FALSE;
-        gPulseEnd=0;
-        digitalWrite(CLOCK_PIN,LOW);
-      }
-    }
-  }
-//are we flashing the LED?
-  if(gLEDOn && gLEDCount==LED_COUNT)
-  {
-//    digitalWrite(LED_BUILTIN, HIGH );
-    pinMode( LED_BUILTIN_TX, OUTPUT);
-    digitalWrite(LED_BUILTIN_TX, LOW );
-  }
- 
-// Read the incoming MIDI bytes and trap for start,stop,continue and clock signals  
+//get 'now'
+  gNow=millis();
+
+ // Read the incoming MIDI bytes and trap for start,stop,continue and clock signals  
   rx = MidiUSB.read();
   if (rx.header!=0)
   {
-  
 //check for clocks, starts and stops
     switch(rx.byte1)
     {
-//-------------------  
 //Clock
       case 0xF8:           
         gotClock();
@@ -166,30 +126,103 @@ void loop()
 #ifdef DEBUG_OUTPUT
         Serial.write("Stop\r\n");
 #endif
-        gLastMilli=0;
         gClockRunning=FALSE;
-        gRunningClock=FALSE;
+        gLastMIDIClock=0;
         break;
     }
   //ignore all other MIDI data
   }
- 
+
+//see if we're triggering the square wave
+  if(gTriggerSquare)
+  {
+//we are, so set a few flags
+    gTriggerSquare=FALSE;
+    gSquareRunning=TRUE;
+    gPulseStart=gNow;
+    gPulseHigh=TRUE;
+//set output to high
+    setPulse();
+    gCurrentNoPulses++;
+  }
+//is the pulse running?
+  if(gSquareRunning)
+  {
+//it is, see if we've reached end of full cycle
+    if(gNow>=(gPulseStart+gFullCycle))
+    {
+      gSquareRunning=FALSE;
+//retrigger if the clock is running, but make sure we haven't issued too many pulses for this clock input
+//this can happen on initial clock signal where we guess the BPM, if real BPM is significantly slower we would issues too many pulses
+      if(gClockRunning&&(gCurrentNoPulses<gPulsesPerMIDI))
+        gTriggerSquare=TRUE;
+    }
+    else
+    {
+//not end of cycle, see if half cycle
+      if(gNow>=(gPulseStart+gDutyCycle))
+      {
+//end of duty cycle - set pulse low
+        gPulseHigh=FALSE;
+        setPulse();
+      }
+    }
+  }
 //LED on ?
   if(gLEDOn)
   {
-//if it's on, decrease the counter
-    gLEDCount--;
-    if(!gLEDCount)
+//see if it's time to turn it off
+    if(gNow>gLEDOffTime)
     {
-//hit zero, turn off the LED
-//      digitalWrite(LED_BUILTIN, LOW );
+//LED flash time complete, turn it off
       pinMode(LED_BUILTIN_TX,INPUT);
       gLEDOn=FALSE;
     }
-
   }
-//Check if we need to send a sync pulse
-  checkSync();
+}
+
+void setPulse()
+{
+   digitalWrite(CLOCK_PIN,gPulseHigh);
+}
+
+void gotClock()
+{
+   unsigned long diff;
+
+//got a MIDI clock, this should always trigger a square, if we're running
+  if(gClockRunning)
+    gTriggerSquare=TRUE;
+//do we have a previous clock?
+  if(gLastMIDIClock==0)
+//we don't - so this is the first MIDI clock message we've recieved, if we guess around 120bpm
+//each 24 ppq signal will be 20ms apart - so let's use that for the first pulse
+    gLastMIDIClock=gNow-gGuessClock;
+//get time between MIDI clocks
+  diff=(gNow-gLastMIDIClock);
+//reset the number of pulses
+  gCurrentNoPulses=0;
+//work out duty cycle of square wave based on PPQN conversion of MIDI clock
+  switch(gClockSetting)
+  {
+    case SYNC_48:
+      gFullCycle=diff>>1; //48ppq (Linn etc)
+      gPulsesPerMIDI=2;
+      break;
+    case SYNC_96:
+      gFullCycle=diff>>2; //96ppq (Oberheim etc)
+      gPulsesPerMIDI=4;
+      break;
+    default:
+      gPulsesPerMIDI=1;
+      gFullCycle=diff; //24ppq (Roland, Korg etc, matches MIDI clock)
+  }
+//the square wave will be 1/2 whatever cycle we've determined
+  gDutyCycle=gFullCycle>>1;
+//record this as the last MIDI clock
+  gLastMIDIClock=gNow;
+
+  gFullCycle-=SQUARE_BUFFER;
 }
 
 //handle start
@@ -198,159 +231,22 @@ void gotStart()
 #ifdef DEBUG_OUTPUT
   Serial.write("Start\r\n");
 #endif
+  gLastMIDIClock=0;
   gClockRunning=TRUE;
-  gClockCount=0;
-  gFlashCnt=0;
-//did we already have a running clock? (i.e. clock is running prior to start)
-  if(!gRunningClock)
-  {
-//no running clock, which means we won't have be able to work out time between ticks
-//and any multiplier out ticks (i.e. larger than 24) will be off
-//so... send the missing ticks prior to this, meaning the last "real tick" will trigger the compelte quarternote on drum machine
-//i.e. 24 ppqn in, 48 ppqn out, so we send 1 tick now, then an additional tick per clock pulse, this means the last "real" 24 ppqn tick will trigger
-//the 48 ppqn signal and will remain in time
-    switch (gClockMultiplier)
-    {
-      case 1:
-        break; //24 ppqn - no need for extra pulse
-      case 2:
-        sendSync(); //48 ppqn - need to send additional pulse
-        break;
-      case 3: // 96 ppqn - should send 2 pulses, leaving for moment as can't test with real DM
-        break;
-    }
-  }
-
-}
-//start sending a square wave / pulse
-void sendPulse()
-{
-  gPulseOn=TRUE;
-  gPulseEnd=0;
 }
 
-//handles incoming midi clock signals
-void gotClock()
-{
-//get the current time
-  unsigned long time=millis();
-//indicate a running clock
-  gRunningClock=TRUE;
-//flag we got a tick
-  gGotInTick=TRUE;
-#ifdef DEBUG_OUTPUT
-  if(gOutTickCount!=0)
-  {
-    snprintf(gOutBuffer,"ERROR - got clock in but out tick not zero (%)\r\n",gOutTickCount);
-    Serial.write(gOutBuffer);
-  }
-#endif
-
-//
-  gClockCount++;
-//set the number of out ticks from this "in tick"  
-  gOutTickCount+=gClockMultiplier;
-//get gap since last tick
- // if(gLastMilli!=0)
- // {
-  //  gMilliGap=time-gLastMilli;
-#ifdef DEBUG_OUTPUT
-  //  snprintf(gOutBuffer, sizeof(gOutBuffer), "Last Milli gap set to  %d \r\n", gMilliGap);
-  //  Serial.write(gOutBuffer);
-#endif
-//  }
- // else
-//  {
-//this is to handle when the clock is only running after a start message
-//In that case - the fist set of additional pulses after the first midi clock will be missing
-//(as there has been no steady clocks before "START" to work out the gap between pulses / the tempo)
-//and things will slowly go out of time
-//so instead we put in a default gap at around 165bpm - this will generate the first additional pulse(s)
-//and the tempo will be corrected by the second clock pulse   
-    gMilliGap=DEFAULT_GAP;
-//note that if clock signals ARE being generated prior to the start signal - this will not cause issues
-//as it will be corrected automatically after 2nd clock pulse anyway
-  // }
- //get the gap between our "out ticks"
-  gOutTickGap=gMilliGap/gClockMultiplier;
-  gLastMilli=time;
-  
-//check if we've hit the 24ppq MIDI clocks
-   if(gClockCount==MIDI_CLOCKS)
-   {
-      gClockCount=0;
-#ifdef BLINK_ON_INPUT
-//see if we're running or not
-      if(gClockRunning)
-        flash();
-#endif
-   }
-}
-
-//handles sending sync signal
-void checkSync()
-{
-//if we got an in tick, just prior to this - send
-  if(gGotInTick)
-  {
-    gGotInTick=FALSE;
-    if(gOutTickCount)
-      sendSync();
-  }
-  else
-  {
-  //haven't recieved an in tick, see if we have pending out ticks
-      if(gOutTickCount)
-      {
-//check time between now and last send
-        unsigned long now=millis();
-        unsigned long timeDiff=now-gLastOutTick;
-        if(timeDiff>=gOutTickGap)
-            sendSync();
-      }
-  }
-
-}
-
-void sendSync()
-{
-  gFlashCnt++;
-//record the time we sent this
-  gLastOutTick=millis();
-//decrease the number of out ticks
-  if( gOutTickCount>0)
-    gOutTickCount--;
-
-//if we're running, send a square wave to the output pin
-  if(gClockRunning)
-    sendPulse();
-//if we've hit PPQ, flash
-  if(gFlashCnt==MIDI_CLOCKS*gClockMultiplier)
-  {
-    if(gClockRunning)
-    {
-        flash();
-#ifdef DEBUG_OUTPUT
-        Serial.write("x\r\n");
-#endif
-    }
-    gFlashCnt=0;
-  }
-}
-
+//flash LED
 void flash()
 {
 //check if we're already 'flashing'
   if(!gLEDOn)
   {
-//we aren't, so set the LED var to on and initialse the counter
+//not currently 'flashing', so flash LED
+    pinMode( LED_BUILTIN_TX, OUTPUT);
+    digitalWrite(LED_BUILTIN_TX, LOW );
+//set the LED var to on and initialse the counter
     gLEDOn=TRUE;
-    gLEDCount=LED_COUNT;
+    gLEDOffTime=millis()+LED_FLASH_TIME;
   }
 }
-//------------------------------------------------------------------------------
-
-
-
-
 
